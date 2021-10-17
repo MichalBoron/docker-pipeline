aec59d50585348f9950128ebc77d0bc1d2ca68c8
MichalBoron <mboron@cs.put.poznan.pl>
2021-10-17 22:41:02 +0200

    Add FlaskApp pipeline to jcasc, fix jcasc terraform

--- phase2/main.tf
+++ phase2/main.tf
@@ -9,17 +9,20 @@ terraform {
 
 provider "docker" {}
 
-
-resource "docker_image" "jcasc" {
-  name  = "localhost:5000/jenkins-casc"
+resource "docker_image" "dockerjcasc" {
+  name  = "localhost:5000/docker-and-jenkins-casc"
   keep_locally = false
 }
 
-resource "docker_container" "jcasc" {
-  image   = docker_image.jcasc.latest
+resource "docker_container" "dockerjcasc" {
+  image   = docker_image.dockerjcasc.latest
   name = "JenkinsContainer"
   networks_advanced {
     name = "simple_pipeline_network"
+  }
+  volumes {
+    host_path = "/var/run/docker.sock"
+    container_path = "/var/run/docker.sock"
   }
   ports {
     internal = 8080
