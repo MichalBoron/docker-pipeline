7674ed04bab125bf4205585c04bc042922a1b965
MichalBoron <mboron@cs.put.poznan.pl>
2021-10-17 12:33:41 +0200

    Add resources for docker docker container.
    This container will be used as a Jenkins agent, to build docker
    containers for applications.

--- phase1/main.tf
+++ phase1/main.tf
@@ -23,3 +23,20 @@ resource "docker_container" "registry" {
     external = 5000
   }
 }
+
+# this image will be used as Jenkins agent to build docker containers
+resource "docker_image" "docker" {
+  name = "docker:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "docker" {
+  image = docker_image.docker.latest
+  name = "dockerworker"
+  restart = "always"
+  tty = true # prevent exiting after startup
+  volumes {
+    host_path = "/var/run/docker.sock"
+    container_path = "/var/run/docker.sock"
+  }
+}
